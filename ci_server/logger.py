04ad4c17af1edf48454604b0b310c53256fc1d33
Seb <94540990+sebastianmonten@users.noreply.github.com>
2024-02-08 17:04:24 +0100

    Feature/16/build logger (#17)
    feat: build history specific logger 
    
    add build_logger in Logger class that only writes logs at INFO level for builds in the directory build_history
    
    add main guard to logger.py
    
    add tests for build history in test_logger.py
    
    ---------
    
    Co-authored-by: sebastianmonten <94540990+sebastianmonten@users.noreply.github>
    Co-authored-by: rasdan <rasdan@kth.se>

--- ci_server/logger.py
+++ ci_server/logger.py
@@ -3,6 +3,19 @@ if __name__ == '__main__': exit()
 import logging
 from datetime import datetime
 
+
+
+import os
+
+class InfoOnlyFilter(logging.Filter):
+    ''' 
+        A filter for enforcing that the logger
+        build_logger can only log at INFO level
+    '''
+    def filter(self, record):
+        return record.levelno == logging.INFO
+
+
 class Logger():
     ''' 
         A Logger class providing functions that log
@@ -16,43 +29,67 @@ class Logger():
 
         # The log message format
         log_str_format = '%(asctime)s - %(levelname)s: %(message)s'
-        
-        # Create a logger and set it to write to todays logfile and 
-        # to use a format with timestamp
-        logger    = logging.Logger        ( "Log" )
-        handler   = logging.FileHandler   ( f"log/{filename}" )
-        formatter = logging.Formatter     ( log_str_format )
-        handler.setFormatter ( formatter )
-        logger.addHandler    ( handler )
-        self.logger = logger
-    def __del__(self): self._close()
+        build_log_str_format = '%(asctime)s: %(message)s' # Remove INFO string from printout
+
+        # Create a logger for standard log levels
+        self.logger = logging.getLogger("Log")
+        self.logger.setLevel(logging.DEBUG) # allows all log levels 
+        handler = logging.FileHandler(f"log/{filename}")
+        formatter = logging.Formatter(log_str_format)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+        # Create a logger for BUILD level logs
+        self.build_logger = logging.getLogger("Build")
+        self.build_logger.setLevel(logging.INFO)  # Only allow INFO level for BUILD
+        build_handler = logging.FileHandler(f"build_history/build_{filename}")
+        build_formatter = logging.Formatter(build_log_str_format)
+        build_handler.setFormatter(build_formatter)
+        build_handler.addFilter(InfoOnlyFilter())  # Add the custom filter that only allows INFO level logs
+        self.build_logger.addHandler(build_handler)
+
+    def __del__(self):
+        self._close()
+
     def _close(self):
         ''' Close all handlers. Use with care, mainly for testing purposes. '''
-        [handler.close() for handler in self.logger.handlers]
+        # for handler in self.logger.handlers + self.build_logger.handlers:
+        #     handler.close()
+
+        # test: trying to find why I get AttributeError: 'Logger' object has no attribute 'build_logger'
+        handlers_to_close = self.logger.handlers
+        if hasattr(self, 'build_logger'):
+            handlers_to_close += self.build_logger.handlers
+        for handler in handlers_to_close:
+            handler.close()
+
     def debug(self, msg, *args): 
         ''' Log message at level DEBUG '''   
-        self.logger.debug (msg, *args)
+        self.logger.debug(msg, *args)
+
     def info(self, msg, *args):    
-        ''' Log message at level INFO'''   
-        self.logger.info  (msg, *args)
+        ''' Log message at level INFO'''
+        self.logger.info(msg, *args)
+
     def warning(self, msg, *args): 
         ''' Log message at level WARNING'''   
-        self.logger.warning  (msg, *args)
+        self.logger.warning(msg, *args)
+
     def error(self, msg, *args):   
         ''' Log message at level ERROR'''   
-        self.logger.error (msg, *args)
-    def log_build(self, build_info : dict):
+        self.logger.error(msg, *args)
+
+    def log_build(self, build_info: dict):
         ''' 
             Log a build status.
             
-            <date time> - INFO: BUILD <commit_id> SUCCESS/FAILURE: 
+            <date time> - BUILD: BUILD <commit_id> SUCCESS/FAILURE: 
                 <status_msg>
 
             :param build_info: A dictionary containng 
                 'success : bool' -- whether build was a success or not
                 'commit_id : str' -- the id of the commit tested
                 'status_msg : str' -- message containing, for example, error message.
-
         '''  
         success = build_info["success"]
         commit_id = build_info["commit_id"]
@@ -61,11 +98,8 @@ class Logger():
         log_str = f"BUILD {commit_id} "
         log_str += "SUCCESS" if success else "FAILURE"
         log_str += ": \n" + "\n".join(
-                    ['\t' + line for line in status_msg.splitlines()]
-                )
-       
-        self.info(log_str)
+            ['\t' + line for line in status_msg.splitlines()]
+        )
 
-
-
-
+        self.build_logger.info(log_str) # can only log at this level, any other level is filtered out
+        self.logger.info(log_str) # can also log at DEBUG, ERROR and WARNING level
--- test_ci_server/test_logger.py
+++ test_ci_server/test_logger.py
@@ -1,20 +1,36 @@
+# #######################################################
+# # added this block to solve ModuleNotFoundError
+# #######################################################
+# import sys
+# import os
+# # Get the absolute path of the parent directory
+# project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# # Add the parent directory to sys.path
+# sys.path.append(project_dir)
+# # Print out sys.path to verify that the directory has been added
+# # print(sys.path)
+# #######################################################
+
+
+
+
 import datetime
 import unittest
 import ci_server.logger
 
+
 class TestLogger(unittest.TestCase):
     
-    file_path = datetime.datetime.now().strftime("log/%Y-%m-%d.test.log")
-
-    def setUp(self) -> None:
-        self.logger = ci_server.logger.Logger(test=True)
+    log_file_path = datetime.datetime.now().strftime("log/%Y-%m-%d.test.log")
+    build_file_path = datetime.datetime.now().strftime("log/%Y-%m-%d.test.log")
+    logger = ci_server.logger.Logger(test=True)
 
     def test_file_path(self):
         ''' 
             Test if the logfile has been created and named after todays date.
         '''
         try: 
-            f = open(self.file_path)
+            f = open(self.log_file_path)
             f.close()
         except FileNotFoundError as e: self.fail(str(e) + "Can't open logfile")
 
@@ -24,7 +40,7 @@ class TestLogger(unittest.TestCase):
             given in logger.py
         '''
 
-        f = open(self.file_path)
+        f = open(self.log_file_path)
         f.seek(0,2)
         self.logger.debug("test1")
         self.logger.info("test2")
@@ -44,7 +60,7 @@ class TestLogger(unittest.TestCase):
         ''' 
             Tests if the logger prints the timestamp in the correct format.
         '''
-        f = open(self.file_path)
+        f = open(self.log_file_path)
         f.seek(0,2)
         self.logger.info("42")
         l = f.readline()
@@ -56,9 +72,11 @@ class TestLogger(unittest.TestCase):
         finally: f.close()
 
 
-
-
-
-
-
-
+    def test_build_logger(self):
+        f = open(self.build_file_path)
+        f.seek(0,2)
+        build_data = {"success" : True, "commit_id" : "123456", "status_msg" : "some message"}
+        self.logger.log_build(build_data)
+        data = f.read() 
+        f.close()
+        self.assertTrue("SUCCESS" in data and "123456" in data and "some message" in data)
