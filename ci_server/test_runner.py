7ed8dedd076cfefc2cf73b012c461e20cc485d69
Stenmarken <67392286+Stenmarken@users.noreply.github.com>
2024-02-08 17:26:56 +0100

    Add logic for the CI server (#18)
    * feat: Add server for the CI server
    
    Motivation: The CI server needs logic for cloning the repo, checking out
    the correct commit, and then running the tests. This is what this commit
    will do.
    
    Tests: Tested by running python3 ci_server/ci_logic.py to verify that
    it's cloning, checking out the commit, logging, and removing the repo
    when finished.
    
    Resolves #11

--- ci_server/test_runner.py
+++ ci_server/test_runner.py
@@ -3,16 +3,14 @@
 Runs all unit and system tests in the directory 'test_ci_server' in tmp with file prefix "test_". 
 This also includes files in subdirectories
 '''
-import unittest
 import os
+import subprocess
 
 CURRENT_PATH = os.path.abspath(__file__)
-TEST_CI_PATH = os.path.join(os.path.dirname(CURRENT_PATH), "../tmp/test_ci_server")
+TMP_CI_PATH = os.path.join(os.path.dirname(CURRENT_PATH), "../tmp/")
 
 def run_tests():
-    loader = unittest.TestLoader()
-    tests = loader.discover(TEST_CI_PATH)
-
-    test_runner = unittest.TextTestRunner()
-    result = test_runner.run(tests)
-    return result
+    os.chdir(TMP_CI_PATH)
+    cmd = ['python3', '-m', "unittest", "discover", "test_ci_server/"]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.decode("utf-8"), result.stderr.decode("utf-8")
--- test_ci_server/test_compilation.py
+++ test_ci_server/test_compilation.py
@@ -3,11 +3,6 @@ import unittest
 
 class CompilationTests(unittest.TestCase):
     def test_compile_file(self):
-        # TODO: Uncomment and add the path when there are files to test
-        """
-        path = ""
+        path = "ci_server/logger.py"
         result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
-
         self.assertTrue(result.returncode == 0, result.stderr)
-        """
-        pass 
