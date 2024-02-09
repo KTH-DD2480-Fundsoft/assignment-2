0b1b5c27b4f1b5d1a54d402ca2c18c1930269971
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2024-02-09 12:03:29 +0100

    feat: repeat compile test for all ci_server python files

--- test_ci_server/test_compilation.py
+++ test_ci_server/test_compilation.py
@@ -2,7 +2,23 @@ import subprocess
 import unittest
 
 class CompilationTests(unittest.TestCase):
-    def test_compile_file(self):
+
+    def test_compile_ci_logic(self):
+        path = "ci_server/ci_logic.py"
+        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
+        self.assertTrue(result.returncode == 0, result.stderr)
+
+    def test_compile_logger(self):
         path = "ci_server/logger.py"
+        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
+        self.assertTrue(result.returncode == 0, result.stderr)
+
+    def test_compile_server(self):
+        path = "ci_server/server.py"
+        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
+        self.assertTrue(result.returncode == 0, result.stderr)
+    
+    def test_compile_test_runner(self):
+        path = "ci_server/test_runner.py"
         result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
         self.assertTrue(result.returncode == 0, result.stderr)
