0d216ca5e85da67dcf7c59f4a3d40e2d7bac9ce7
rasdan <rasdan@kth.se>
2024-02-10 14:37:05 +0100

    fix: simpler approach

--- test_ci_server/test_compilation.py
+++ test_ci_server/test_compilation.py
@@ -1,24 +1,11 @@
-import subprocess
 import unittest
+import compileall
 
 class CompilationTests(unittest.TestCase):
 
     def test_compile_ci_logic(self):
-        path = "ci_server/ci_logic.py"
-        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
-        self.assertTrue(result.returncode == 0, result.stderr)
-
-    def test_compile_logger(self):
-        path = "ci_server/logger.py"
-        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
-        self.assertTrue(result.returncode == 0, result.stderr)
-
-    def test_compile_server(self):
-        path = "ci_server/server.py"
-        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
-        self.assertTrue(result.returncode == 0, result.stderr)
-    
-    def test_compile_test_runner(self):
-        path = "ci_server/test_runner.py"
-        result = subprocess.run(['python', '-m', 'py_compile', path], capture_output=True, text=True)
-        self.assertTrue(result.returncode == 0, result.stderr)
+        try:     
+            compileall.compile_dir("ci_server", force=True)
+            pass
+        except Exception as e:
+            self.fail(f"Compilation error: {str(e)}")
