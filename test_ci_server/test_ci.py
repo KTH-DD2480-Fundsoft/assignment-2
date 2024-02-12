4e0b618f7caf520675157c5b35a7ebaab7c23320
rasdan <rasdan@kth.se>
2024-02-12 12:44:54 +0100

    passing tests

--- test_ci_server/test_ci.py
+++ test_ci_server/test_ci.py
@@ -3,7 +3,7 @@ import unittest
 class CIUnitTests(unittest.TestCase):
     def test_return_true(self):
         return True
-    def test_fail(self):
-        self.fail("This is supposed to fail")
-    def test_error(self):
-        raise Exception("This is an error")
+    #def test_fail(self):
+    #    self.fail("This is supposed to fail")
+    #def test_error(self):
+    #    raise Exception("This is an error")
