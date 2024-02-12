11d4d7252601df7b1fbf66d1efe2da8dede97707
Victor Stenmark <victor.stenmark22@gmail.com>
2024-02-12 16:41:08 +0100

    Add documentation for test_ci_server

--- test_ci_server/test_server.py
+++ test_ci_server/test_server.py
@@ -5,9 +5,14 @@ import time
 
 
 class TestServer(unittest.TestCase):
-   
+    '''
+    Class containing tests for the flask server in ci_server/server.py
+    '''
     @classmethod
     def setUpClass(cls) -> None:
+        '''
+        Hook method for setting up class fixture before running tests in the class.
+        '''
         cls.ip = "0.0.0.0"
         cls.port = 5000 
         
@@ -26,18 +31,41 @@ class TestServer(unittest.TestCase):
         cls.app = app  
         cls.server = Process(target=start_server, args=(cls.ip, cls.port))
         cls.server.start()
-        time.sleep(1)        
-
+        time.sleep(1)  
+    
     @property 
-    def valid_authkey(self): 
+    def valid_authkey(self):
+        '''
+        
+        
+        Returns
+        -------
+        self.new_authkey: (str)
+            
+        ''' 
         return self.new_authkey
 
     @property 
     def invalid_authkey(self): 
+        '''
+        
+
+        Returns
+        -------
+            "letmein": (str)
+        '''
         return "letmein"
     
     @property
     def valid_data(self):
+        '''
+        
+
+        Returns 
+        -------
+        data: (dict)
+            A dictionary containing branch, commit, and push info.
+        '''
         data = {} 
         data['ref'] = "ref/this/branch/doesnt/exist"
         data['head_commit'] = {
@@ -49,6 +77,9 @@ class TestServer(unittest.TestCase):
         return data
 
     def test_webhook_bad_authkey(self):
+        '''
+        Tests that an invalid AUTH_KEY results in a HTPP 401 response code.
+        '''
         headers = {"X-Hub-Signature-256" : self.invalid_authkey}
         with self.app.test_client() as client: 
             response = client.post(self.webhook_addr, json={}, headers=headers)
@@ -56,6 +87,10 @@ class TestServer(unittest.TestCase):
                             msg=f"Expected HTTP 401 for bad authkey, got {response.status_code}")
     
     def test_webhook_bad_json(self):
+        '''
+        Tests that a webhook request with a valid AUTH_KEY but invalid JSON results 
+        in a HTTP 400 response code.
+        '''
         payload = {"mol" : 42}
         headers = {"X-Hub-Signature-256" : self.valid_authkey}
         with self.app.test_client() as client:
@@ -64,6 +99,9 @@ class TestServer(unittest.TestCase):
                             msg=f"Expected HTTP 400 for bad json, got {response.status_code}")
     
     def test_webhook(self):
+        '''
+        Tests that posting a valid webhook request results in a HTTP 202 response code.
+        '''
         headers = {"X-Hub-Signature-256" : self.valid_authkey}
         with self.app.test_client() as client:
             response = client.post(self.webhook_addr,
@@ -73,6 +111,10 @@ class TestServer(unittest.TestCase):
                             msg=f"Expected HTTP 202, got {response.status_code}") 
 
     def test_index(self):
+        ''' 
+        Tests that doing a get request for the index address results in a
+        HTTP 200 response code.
+        '''
         with self.app.test_client() as client:
             response = client.get(self.index_addr)
             self.assertTrue(response.status_code == 200, 
@@ -81,6 +123,12 @@ class TestServer(unittest.TestCase):
     @unittest.skipIf(not os.path.isdir('build_history'), 
                      "Build history directory not found. Skipping test.")
     def test_build_history_urls(self):
+        '''
+        Tests that each file in build_history has a valid URL that is equal 
+        to its file name. Does this by performing a get request for each url
+        and verifying that it returns a HTTP 200 response code. This test
+        is skipped if the build_history directory doesn't exist.
+        '''
         # Get all filenames in build_history
         build_history_names = [f for f in os.listdir('build_history') 
                                if os.path.isfile(os.path.join('build_history', f))]
@@ -94,6 +142,9 @@ class TestServer(unittest.TestCase):
     
     @classmethod
     def tearDownClass(cls) -> None:
+        '''
+        Hook method for deconstructing the class fixture after running all tests in the class.
+        '''
         cls.server.terminate()
         cls.server.join()
         time.sleep(1)
