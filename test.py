6a13a8dc2a28d0000be5aff4ec93065608555cc3
rasdan <rasdan@kth.se>
2024-02-12 09:52:52 +0100

    test with wrapper testrunner

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+if __name__ == '__main__':
+    loader = unittest.TestLoader()
+    tests = loader.discover("test_ci_server")
+    runner = unittest.TextTestRunner()
+    res =  runner.run(tests)
+    errors = [f"Error in {type(tc).__name__}:{err}\n" for tc,err in res.errors]
+    failures = [f"Error in {type(tc).__name__}:{err}\n" for tc,err in res.errors]
+    print((errors,failures))
+
+
+
